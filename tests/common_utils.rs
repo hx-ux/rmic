51808406848e8791808f4d78d10cb89684d9bcb0
hxux <hxux>
2026-06-06 22:45:36 +0200

    added testcase for get version

---
+++ tests/common_utils.rs
@@ -0,0 +1,27 @@
+use rmic::{Gmic, GmicError};
+
+use crate::utils::process_images;
+mod utils;
+#[test]
+fn check_dryrun() {
+    let name = "water_params";
+    let effect = |gmic: Gmic| gmic.add_raw_filter("polaroid 5,30");
+    let result = process_images(name, effect);
+
+    let mut command = "".to_string();
+    if let Ok(c) = result {
+        command = c.to_string();
+    }
+    assert_eq!(
+        "-input input.jpg polaroid 5,30 -output tests/out/water_params.jpg".to_string(),
+        command
+    );
+}
+
+#[test]
+fn check_version() {
+    let gmic = Gmic::new();
+    if let Some(v) = gmic.version() {
+        assert_eq!("376", v)
+    }
+}
